4366b205060bc8d9f58d2c0e558c0c6bbd00a28c
Roman Mescheryakov <you@example.com>
2020-02-09 03:59:12 +0400

    сборка

---
+++ build/precache-manifest.99eb72cf5fb7a38a9b27da9e6fc2a1bf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9593f8780dff5a54201e7fb3586c4be6",
+    "url": "/build/index.html"
+  },
+  {
+    "revision": "dc9e96060076e013c6a9",
+    "url": "/build/static/css/2.73d444c6.chunk.css"
+  },
+  {
+    "revision": "03eca52c81ca571a4b4d",
+    "url": "/build/static/css/main.ee5631fe.chunk.css"
+  },
+  {
+    "revision": "dc9e96060076e013c6a9",
+    "url": "/build/static/js/2.707df119.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/build/static/js/2.707df119.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03eca52c81ca571a4b4d",
+    "url": "/build/static/js/main.209caf56.chunk.js"
+  },
+  {
+    "revision": "9751401102b2f6c4ad51",
+    "url": "/build/static/js/runtime-main.be65c761.js"
+  }
+]);
