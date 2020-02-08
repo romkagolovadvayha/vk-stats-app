5011834be412c7056976d13d949a038419e10fc7
Roman Mescheryakov <you@example.com>
2020-02-09 03:54:44 +0400

    сборка

---
+++ build/precache-manifest.d99bbfad85b69a8946f1c5a6bffe0334.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bafce197905f8375fa0e521e8179f9c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "994b019b04d10af6d664",
+    "url": "/static/css/2.73d444c6.chunk.css"
+  },
+  {
+    "revision": "03eca52c81ca571a4b4d",
+    "url": "/static/css/main.ee5631fe.chunk.css"
+  },
+  {
+    "revision": "994b019b04d10af6d664",
+    "url": "/static/js/2.8f5237c8.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.8f5237c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03eca52c81ca571a4b4d",
+    "url": "/static/js/main.209caf56.chunk.js"
+  },
+  {
+    "revision": "408d2366ea3ccba2b505",
+    "url": "/static/js/runtime-main.cdc87acb.js"
+  }
+]);
