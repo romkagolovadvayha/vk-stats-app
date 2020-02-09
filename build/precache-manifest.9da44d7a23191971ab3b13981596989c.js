7c0f5cb903914ac6c0a5ee98d5e4bee7bc7e37e8
Roman Mescheryakov <you@example.com>
2020-02-09 13:11:28 +0400

    сборка

---
+++ build/precache-manifest.9da44d7a23191971ab3b13981596989c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "845d2f6f167b6e262035cf61ee526863",
+    "url": "/vk-stats-app/build/index.html"
+  },
+  {
+    "revision": "b49cb3fb43bf67585c29",
+    "url": "/vk-stats-app/build/static/css/2.73d444c6.chunk.css"
+  },
+  {
+    "revision": "74c0b5583618fbfa9423",
+    "url": "/vk-stats-app/build/static/css/main.ee5631fe.chunk.css"
+  },
+  {
+    "revision": "b49cb3fb43bf67585c29",
+    "url": "/vk-stats-app/build/static/js/2.5d7408c0.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/vk-stats-app/build/static/js/2.5d7408c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74c0b5583618fbfa9423",
+    "url": "/vk-stats-app/build/static/js/main.1c1a8e7c.chunk.js"
+  },
+  {
+    "revision": "b0c55b9cee54fb570b08",
+    "url": "/vk-stats-app/build/static/js/runtime-main.13aab91f.js"
+  }
+]);
