0dab68a1a2bfeb947ac306835cefa33bb23ab1a7
Roman Mescheryakov <rom_ik@bk.ru>
2020-02-10 08:04:53 +0000

    Updates

---
+++ precache-manifest.fbec62a8b1fe196888ed7d27bdb037b8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a54d13b1bb4a746df9eb3a1c5e7a627",
+    "url": "/vk-stats-app/build/index.html"
+  },
+  {
+    "revision": "7893c5f28fccbef3c753",
+    "url": "/vk-stats-app/build/static/css/2.73d444c6.chunk.css"
+  },
+  {
+    "revision": "a4b5781ff6998212381c",
+    "url": "/vk-stats-app/build/static/css/main.163a9d4d.chunk.css"
+  },
+  {
+    "revision": "7893c5f28fccbef3c753",
+    "url": "/vk-stats-app/build/static/js/2.119dcd7a.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/vk-stats-app/build/static/js/2.119dcd7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4b5781ff6998212381c",
+    "url": "/vk-stats-app/build/static/js/main.ef59302a.chunk.js"
+  },
+  {
+    "revision": "b0c55b9cee54fb570b08",
+    "url": "/vk-stats-app/build/static/js/runtime-main.13aab91f.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4a267b81ceb7d394f3d91423b159db6d.js"
+  "/vk-stats-app/build/precache-manifest.fbec62a8b1fe196888ed7d27bdb037b8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/vk-stats-app/build/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
